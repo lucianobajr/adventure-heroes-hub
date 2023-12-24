30e8a92c9a6eb3afce1d5733a0f7aa130271c46b
lucianobajr <luciano.alcantara@ufv.br>
2023-12-23 23:15:10 -0300

    add(+): finish

--- src/components/modal-battle/index.tsx
+++ src/components/modal-battle/index.tsx
@@ -1,59 +1,73 @@
 import React from "react";
 import { IPokemonCard } from "@/interfaces/pokemon/pokemon-card";
+import { Button } from "../ui/button";
 
 interface ModalBattleProps {
   heroes: IPokemonCard[];
   closeModal: () => void;
 }
 
-const ModalBattle: React.FC<ModalBattleProps> = ({ closeModal, heroes }) => {
-  const scoreHero1 = Number(heroes[0].hp);
-  const scoreHero2 = Number(heroes[1].hp);
-
-  let resultMessage = "";
+const determineWinner = (heroes: IPokemonCard[]): string => {
+  const [hero1, hero2] = heroes;
+  const scoreHero1 = Number(hero1.hp);
+  const scoreHero2 = Number(hero2.hp);
 
   if (scoreHero1 > scoreHero2) {
-    resultMessage = `${heroes[0].name} venceu!`;
+    return `${hero1.name} venceu!`;
   } else if (scoreHero1 < scoreHero2) {
-    resultMessage = `${heroes[1].name} venceu!`;
+    return `${hero2.name} venceu!`;
   } else {
-    resultMessage = "A batalha terminou em empate!";
+    return "A batalha terminou em empate!";
   }
+};
+
+const ModalBattle: React.FC<ModalBattleProps> = ({ closeModal, heroes }) => {
+  const resultMessage = determineWinner(heroes);
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-80 flex items-center justify-center">
-      <div className="bg-white p-4 rounded-lg w-96">
-        <h2 className="text-2xl font-semibold mb-4">Comparação de Heróis</h2>
+      <div className="bg-white p-8 rounded-lg w-auto">
 
-        <div className="flex justify-between mb-4">
-          {heroes.map((hero) => (
-            <div key={hero.id}>
-              <h3 className="text-xl font-semibold mb-2">{hero.name}</h3>
+        <div className="flex flex-col justify-between mb-4 items-center">
+          <h3 className="text-6xl font-roboto-slab font-semibold text-center"><span className="underline underline-offset-3 decoration-8 decoration-main dark:dark-main">{resultMessage}</span></h3>
+
+          <div className="flex items-center mt-10">
+            <div className="flex items-center mr-2">
               <img
-                src={hero.images.large}
-                alt={hero.name}
-                className="w-24 h-24 rounded-full"
+                src={heroes[0].images.large}
+                alt={heroes[0].name}
+                className="w-72 h-96 rounded-sm"
+              />
+              <span className="text-2xl font-semibold ml-2">{heroes[0].name}</span>
+            </div>
+            <span className="text-2xl font-semibold mx-2">VS</span>
+            <div className="flex items-center ml-2">
+              <span className="text-2xl font-semibold mr-2">{heroes[1].name}</span>
+              <img
+                src={heroes[1].images.large}
+                alt={heroes[1].name}
+                className="w-72 h-96 rounded-sm"
               />
             </div>
-          ))}
+          </div>
         </div>
 
-        <div className="mb-4">
+
+        <div className="mb-10 p-1 flex justify-between">
           {heroes.map((hero) => (
-            <p key={hero.id}>
-              {hero.name} - Pontuação Total: {hero.hp}
+            <p key={hero.id} className="font-roboto-slab italic">
+              {`Pontuação Total: ${hero.hp}`}
             </p>
           ))}
         </div>
 
-        <h3 className="text-xl font-semibold">{resultMessage}</h3>
 
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+        <Button
+          className='w-full rounded-lg text-base font-roboto-slab font-bold h-12 text-white bg-main hover:bg-dark-main transition-colors duration-300 mt-4'
           onClick={closeModal}
         >
-          Fechar
-        </button>
+          Batalhar novamente!
+        </Button>
       </div>
     </div>
   );
